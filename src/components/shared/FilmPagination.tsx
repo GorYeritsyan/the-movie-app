91feeb8a0d287e3c3fc7c85b81b7032389f93077
GorYeritsyan <gor.k.yeritsyan@gmail.com>
2024-10-21 02:39:53 +0400

    refactor pagination

--- src/components/shared/FilmPagination.tsx
+++ src/components/shared/FilmPagination.tsx
@@ -7,28 +7,54 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "../ui/pagination";
-import { addPage, decreasePageByOne, increasePageByOne } from "@/store/slices/filmsSlice";
+import {
+  addPage,
+  decreasePageByOne,
+  increasePageByOne,
+} from "@/store/slices/filmsSlice";
 import { cn } from "@/lib/utils";
+import { useCallback, useState } from "react";
+import { useGetAllMoviesQuery } from "@/api/api";
 
-const paginationButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const paginationButtons: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const FilmPagination = () => {
   const { page } = useAppSelector((state) => state.filmsReducer);
   const dispatch = useAppDispatch();
+  const { data } = useGetAllMoviesQuery();
 
+  // pagination logic
+
+  let [portionNumber, setPortionNumber] = useState(1);
+  let portionSize = 10;
+  let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+  let rightPortionPageNumber = portionNumber * portionSize;
+
+  function handleClick(button: number){
+    dispatch(addPage(button))
+    scrollTo(0, 0)
+  }
+
+  
   return (
     <Pagination>
       <PaginationContent className="flex gap-x-2">
-        <PaginationItem>
-          <PaginationPrevious onClick={() => dispatch(decreasePageByOne())} className="cursor-pointer text-md active:bg-slate-900" />
-        </PaginationItem>
+        {portionNumber > 1 && (
+          <PaginationItem>
+            <PaginationPrevious
+              onClick={() => dispatch(decreasePageByOne())}
+              className="cursor-pointer text-md active:bg-slate-900"
+            />
+          </PaginationItem>
+        )}
         {paginationButtons.map((button) => (
           <PaginationItem key={button}>
             <PaginationLink
-              onClick={() => dispatch(addPage(button))}
+              onClick={() => handleClick(button)}
               className={cn(
                 " cursor-pointer p-5 active:bg-slate-900",
-                button === page && "bg-green-500 hover:bg-green-600 active:bg-green-700"
+                button === page &&
+                  "bg-green-500 hover:bg-green-600 active:bg-green-700"
               )}
             >
               {button}
@@ -36,7 +62,10 @@ const FilmPagination = () => {
           </PaginationItem>
         ))}
         <PaginationItem>
-          <PaginationNext onClick={() => dispatch(increasePageByOne())} className="cursor-pointer text-md active:bg-slate-900" />
+          <PaginationNext
+            onClick={() => dispatch(increasePageByOne())}
+            className="cursor-pointer text-md active:bg-slate-900"
+          />
         </PaginationItem>
       </PaginationContent>
     </Pagination>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,7 +15,6 @@ const Home = () => {
   return (
       <Container>
         <div className="flex flex-col gap-y-6 pb-8 ">
-          <h2 className="text-4xl ml-16">Discover</h2>
           <div className="flex py-6 flex-wrap items-center justify-center gap-x-8 gap-y-8">
             {isFetching
               ? // <SkeletonCard />
