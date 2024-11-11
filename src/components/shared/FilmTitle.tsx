da4c4c7e16c26dacbef26aece8de3723ccfb8792
GorYeritsyan <gor.k.yeritsyan@gmail.com>
2024-11-11 16:55:37 +0400

    refactor film card

--- src/components/shared/FilmTitle.tsx
+++ src/components/shared/FilmTitle.tsx
@@ -3,11 +3,16 @@ import { FilmCardProps } from "./FilmCard";
 
 const FilmTitle = ({ film }: FilmCardProps) => {
   return (
-    <h3 className="hover:text-green-400 transition-colors duration-200 cursor-pointer text-lg font-semibold">
-      <Link to={`/movie/${film.id}`}>
-        {film.title.length <= 30 ? film.title : film.title.slice(0, 20) + "..."}
-      </Link>
-    </h3>
+    <div className="flex flex-col ">
+      <h3 className="hover:text-green-400 transition-colors duration-200 cursor-pointer text-lg font-semibold">
+        <Link to={`/movie/${film.id}`}>
+          {film.title.length <= 30
+            ? film.title
+            : film.title.slice(0, 20) + "..."}
+        </Link>
+      </h3>
+      <span className="text-sm">{film.vote_average.toFixed(1)} • {film.release_date.split('-').at(0)}</span>
+    </div>
   );
 };
 
